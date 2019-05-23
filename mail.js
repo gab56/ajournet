f50a1993b7f9b06a6747a4852b745a4eecbf3c88
Gabriel Quesada <gab-56@hotmail.com>
2019-05-23 10:06:01 -0600

    Fixes in email call

--- mail.js
+++ mail.js
@@ -1,21 +1,22 @@
 $.ajax({
   type: 'POST',
-  url: 'https://mandrillapp.com/api/1.0/users/info.json',
+  url: 'https://mandrillapp.com/api/1.0//messages/send.json',
   data: {
       "key": "bd947e01fbf6bf69b6f4b2fedfd6ae00-us16",
       "message": {
-      "from_email": "gquesada@pernixlabs.com",
-      "to": [
-          {
-              "email": "gquesada@pernixlabs.com",
-              "name": "Gabriel",
-              "type": "to"
-          }
+        "from_email": "gquesada@pernixlabs.com",
+        "to": [
+            {
+                "email": "gquesada@pernixlabs.com",
+                "name": "Gabriel",
+                "type": "to"
+            }
           ],
-      "autotext": "true",
-      "subject": "Subject",
-      "html": "Content"
-      }
+        "autotext": null,
+        "subject": "Example subject",
+        "html": "<p>Example HTML content</p>",
+        "text": "Example text content"
+        }
   }
   }).done(function(response) {
       console.log(response);
